8cad18e6f7acc5afb9781819f6e3a0ea02c49d10
jashive <68502405+jashive@users.noreply.github.com>
2026-09-03 16:22:43 -0700

    Recover W9 workforce dashboard API

---
+++ api/workforce/dashboard.js
@@ -0,0 +1,20 @@
+import { runWorkforceDashboard } from "../../server-internal/workforce-compliance-dashboard-impl.js";
+import { requireServiceosServerTarget } from "../../src/server/serviceosServerEnvironment.js";
+
+export default async function handler(req, res) {
+  try {
+    requireServiceosServerTarget(process.env, {
+      allowProduction: true,
+      allowNonProduction: true,
+      requireProductionApproval: true,
+    });
+  } catch (error) {
+    return res.status(error.status || 403).json({
+      success: false,
+      error: error.message,
+      code: error.code || "WORKFORCE_SERVER_TARGET_INVALID",
+    });
+  }
+
+  return runWorkforceDashboard(req, res);
+}
